46b156b5a23641f523311cb8ae125fdbfc49f261
admirkrilasevic <admirkrilasevic@gmail.com>
2022-05-16 21:26:41 +0200

    fix: added date format

--- api/config.php
+++ api/config.php
@@ -7,6 +7,27 @@ class Config
 
 	public static function DB_HOST()
 	{
+		return Config::get_env("DB_HOST", "localhost");
+	}
+	public static function DB_USERNAME()
+	{
+		return Config::get_env("DB_USERNAME", "forumibu");
+	}
+	public static function DB_PASSWORD()
+	{
+		return Config::get_env("DB_PASSWORD", "forumibu");
+	}
+	public static function DB_SCHEME()
+	{
+		return Config::get_env("DB_SCHEME", "forumibudb");
+	}
+	public static function DB_PORT()
+	{
+		return Config::get_env("DB_PORT", "3306");
+	}
+
+	public static function DB_HOST()
+	{
 		return Config::get_env("DB_HOST", "127.0.0.1");
 	}
 	public static function DB_USERNAME()
